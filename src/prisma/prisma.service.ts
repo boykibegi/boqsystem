3f541dad67ed6d9bd7f5f62f235f178c466e0efe
hans <hanshandsome23@gmail.com>
2023-10-23 17:27:01 +0300

    update

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,8 @@ export class PrismaService extends PrismaClient {
         super({
             datasources:{
                 db:{
-                    url:'postgres://default:CQs9z7PquDZb@ep-patient-credit-49696121-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15"'
+                    url:'postgres://default:CQs9z7PquDZb@ep-patient-credit-49696121-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15',
+                    
                 }
             }
         })
